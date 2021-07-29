07689d81d8b00c1546b5cf5902ce8c62cc2cd123
amy gallacher <amy.gallacher@codeclan.com>
2021-07-29 12:40:51 +0100

    progress with exercise b

--- start_point/exercise_b_users.py
+++ start_point/exercise_b_users.py
@@ -55,13 +55,47 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+
+print(users["Jonathan"]["twitter"])
+
 # 2. Get Erik's hometown
+
+print(users["Erik"]["home_town"])
 # 3. Get the array of Erik's lottery numbers
+
+print(users["Erik"]["lottery_numbers"])
+
 # 4. Get the species of Avril's pet Monty
+
+print(users["Avril"]["pets"][0]["species"])
+
 # 5. Get the smallest of Erik's lottery numbers
+
+erik_numbers = (users["Erik"]["lottery_numbers"])
+erik_numbers.sort()
+print(erik_numbers[0])
+
 # 6. Return an array of Avril's lottery numbers that are even
+
+avril_numbers = (users["Avril"]["lottery_numbers"])
+
+avril_even_numbers = []
+
+for number in avril_numbers:
+  if (number % 2 == 0):
+    avril_even_numbers.append(number)
+print(avril_even_numbers)
+
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+
+users["Erik"]["lottery_numbers"].append(7)
+print(users["Erik"]["lottery_numbers"])
+
 # 8. Change Erik's hometown to Edinburgh
+
+users["Erik"]["home_town"] = "Edinburgh"
+print(users["Erik"]["home_town"])
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10. Add another person to the users dictionary
 # 11. Print a list of the numbers that appear in both Jonathon and Avril's lottery numbers
